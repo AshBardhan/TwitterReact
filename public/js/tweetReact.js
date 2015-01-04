33c6eeaddb8ee67dcfee3b9a34b443ee4bb82a68
AshishBardhan <ashbardhan@gmail.com>
2015-01-04 20:14:12 +0530

    Adding Design in Tweets

--- public/js/tweetReact.js
+++ public/js/tweetReact.js
@@ -5,10 +5,17 @@ var tweetReact = {
     var Tweet = React.createClass({
       render : function(){
         return (
-            <li class='tweet-item'>
-              <h2>@{this.props.username}</h2>
-              <div class='tweet-msg'>{this.props.text}</div>
-            </li>
+            <div className='row tweet-item'>
+              <div className='col-xs-2 col-sm-1'>
+                <div className='tweet-img'>
+                  <img src={this.props.imgUrl} />
+                </div>
+              </div>
+              <div className='col-xs-10 col-sm-11'>
+                <div className='tweet-user'>@{this.props.username}</div>
+                <div className='tweet-msg'>{this.props.text}</div>
+              </div>
+            </div>
             )
       }
     });
@@ -16,12 +23,12 @@ var tweetReact = {
     var TweetList = React.createClass({
       render : function(){
         var tweetItem = this.props.data.map(function(tweet){
-          return <Tweet text={tweet.text} username={tweet.user.screen_name} />
+          return <Tweet text={tweet.text} username={tweet.user.screen_name} imgUrl={tweet.user.profile_image_url} />
         });
         return (
-          <ul id='tweet-list'>
+          <div id='tweet-list'>
             {tweetItem}
-          </ul>
+          </div>
         )
       }
     });
