49930cad15b677f7c6892efbfeb6ce26c3c35e3a
Ashish Bardhan <ashbardhan@gmail.com>
2021-02-22 22:00:18 +0530

    update dependency and run node server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	path = require('path'),
 	http = require('http'),
-	app = express();
+	app = express(),
+	methodOverride = require('method-override');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -10,7 +11,7 @@ app.set('view engine', 'pug');
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(methodOverride());
 app.use(app.router);
 app.use(require('less-middleware')({src: path.join(__dirname, 'public')}));
 app.use(express.static(path.join(__dirname, 'public')));
